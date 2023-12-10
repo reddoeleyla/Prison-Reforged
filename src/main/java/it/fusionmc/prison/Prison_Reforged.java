87a81174b4b20dc0358be651df265a390ee6558b
reddo <dinardoluca0@gmail.com>
2023-12-10 18:52:48 +0100

    Initial commit

---
+++ src/main/java/it/fusionmc/prison/Prison_Reforged.java
@@ -0,0 +1,17 @@
+package it.fusionmc.prison;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Prison_Reforged extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
